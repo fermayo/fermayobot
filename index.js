0cf23781ea577454f3f61ef3ecf9410d11cedacc
Fernando Mayo <fernando@undefinedlabs.com>
2019-06-25 20:48:32 +0200

    Autoassign creator of PR

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
 module.exports = app => {
-  // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  app.on('pull_request.opened', async context => {
+    const params = context.issue({assignees: [context.payload.pull_request.user.login]})
+    return context.github.issues.addAssignees(params)
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
